26e532641e974cc605d128481a8ef7bfd661f592
sethfork <sethfork@users.noreply.github.com>
2018-10-10 17:28:36 -0400

    Bump rsw3; use new "init" state key

--- src/App.js
+++ src/App.js
@@ -42,12 +42,12 @@ class App extends Component {
 
 const mapDispatchToProps = dispatch => ({
   init: () => dispatch(actions.init.init()),
-  getAccounts: () => dispatch(actions.accounts.getRequest()),
+  getAccounts: () => dispatch(actions.accounts.getRequest())
 });
 
 export default compose(
   connect(
-    (state) => ({
+    state => ({
       initialized: selectors.init.selectInit(state)
     }),
     mapDispatchToProps
@@ -55,11 +55,10 @@ export default compose(
   lifecycle({
     componentDidMount() {
       this.props.init();
-      this.props.getAccounts();
     }
   }),
-  withLoading(({ initialized }) => {
-    console.log(initialized)
-    return true;
-  })
+  withLoading(
+    ({ initialized }) =>
+      !initialized.has("accounts") || initialized.get("isLoading")
+  )
 )(App);
